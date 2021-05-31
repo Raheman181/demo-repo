fb5dfff713db83d3080a2f67d804447dc7f0cb38
Raheman Sheikh <rahemansheikh181@gmail.com>
2021-05-31 15:32:32 +0530

    corrected the indentation error

--- index.py
+++ index.py
@@ -1,16 +1,16 @@
 for i in range(5):
     print("hello world")
-def calc(a, o, b):
-    if o=='+':
-    print(a+b)
-    elif o=='-':
-    print(a-b)
-    elif o=='/':
-    print(a/b)
-    else o=='*':
-    print(a*b)
+def calc(a, e, b):
+    if e=='+':
+       print(a+b)
+    elif e=='-':
+       print(a-b)
+    elif e=='/':
+       print(a/b)
+    elif e=='*':
+       print(a*b)
     return;
 s=input("enter operator +-*/")
 c, d=input("enter two  numbers")
 out=calc(c, s, d)
-Print (out)
+print (out)
